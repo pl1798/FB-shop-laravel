6125783af41e2f0992631869df6667bda546103d
loc son <sploc.work@gmail.com>
2021-05-14 00:44:17 -0700

    php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
 // Tim so lon nhat, lon nhi trong mang so nguyen, khong sap xep, 1 vong lap
 
-int $a=[];
-int $i, $n, $solonnhi;
+$a=[];
+$i=0, $n=0, $solonnhi=0;
 
 for(i=0;i<10;i++){
     if($a[i] < $a[i+1]){
