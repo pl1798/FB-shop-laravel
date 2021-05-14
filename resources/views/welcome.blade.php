e06c3069b282b5af20c0418eb2935cf95e2359ea
loc son <sploc.work@gmail.com>
2021-05-14 00:58:26 -0700

    php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@ $n=0;
 $solonnhi=0;
 
 for($i=0;$i<10;$i++){
+    $a[$i]=$i;
     if($a[$i] < $a[$i+1]){
         if(($solonnhi < $a[$i]) && ($solonnhi < $a[$i+1])) $solonnhi=$a[$i];
         $n =$a[$i+1];
