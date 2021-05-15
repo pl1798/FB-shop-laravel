43a0ed87db2f2f735f3602c765127977a3041e99
pl1798 <sploc.work@gmail.com>
2021-05-15 03:28:21 -0700

    w

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,11 @@
         <!-- Google Font: Source Sans Pro -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
         <!-- Font Awesome Icons -->
-        <link rel="stylesheet" href="{{asset("/css/fontawesome-free/css/all.min.css")}}>
+        <link rel="stylesheet" href="{{secure_asset("/css/fontawesome-free/css/all.min.css")}}>
         <!-- overlayScrollbars -->
-        <link rel="stylesheet" href={{asset("css/overlayScrollbars/css/OverlayScrollbars.min.css")}}>
+        <link rel="stylesheet" href={{secure_asset("css/overlayScrollbars/css/OverlayScrollbars.min.css")}}>
         <!-- Theme style -->
-        <link rel="stylesheet" href={{asset("/css/adminlte.min.css")}} >
+        <link rel="stylesheet" href={{secure_asset("/css/adminlte.min.css")}} >
     </head>
     <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
         <div class="wrapper">
