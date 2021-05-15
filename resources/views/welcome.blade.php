6adfeb68b5e3738063584d747127de2232f13532
pl1798 <sploc.work@gmail.com>
2021-05-15 03:22:27 -0700

    update UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Font Awesome Icons -->
         <link rel="stylesheet" href="{{asset("/css/fontawesome-free/css/all.min.css")}}>
         <!-- overlayScrollbars -->
-        <link rel="stylesheet" href=asset{{"/css/overlayScrollbars/css/OverlayScrollbars.min.css"}}>
+        <link rel="stylesheet" href={{asset("css/overlayScrollbars/css/OverlayScrollbars.min.css")}}>
         <!-- Theme style -->
         <link rel="stylesheet" href={{asset("/css/adminlte.min.css")}} >
     </head>
