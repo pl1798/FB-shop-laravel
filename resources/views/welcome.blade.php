82e5887447edd0cb48976014b413186890101c72
pl1798 <sploc.work@gmail.com>
2021-05-15 03:10:51 -0700

    update UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <!-- overlayScrollbars -->
         <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
         <!-- Theme style -->
-        <link rel="stylesheet" href={{asset("/public/dist/css/adminlte.min.css")}} >
+        <link rel="stylesheet" href={{asset("/css/adminlte.min.css")}} >
     </head>
     <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
         <div class="wrapper">
