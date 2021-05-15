1400bdbd18e01569b35792802dd0f9dfa7de3662
loc son <sploc.work@gmail.com>
2021-05-14 18:42:03 -0700

    php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@ for($i=2;$i<count($a);$i++){
     if($solonnhat < $a[$i]){
         $solonnhat=$a[$i];
     }
-    if($solonnhi<$a[$i] && $solonnhi<$solonnhat){
+    if($solonnhi<$a[$i] && $solonnhi!=$solonnhat){
         $solonnhi=$a[$i];
     }
 }
