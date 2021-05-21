7b4aafd2ab865ee7a8059aa137cc608f2ca1bbb0
pl1798 <sploc.work@gmail.com>
2021-05-21 09:20:23 -0700

    new

--- config/app.php
+++ config/app.php
@@ -228,7 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
 
     ],
 
