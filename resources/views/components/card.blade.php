d0e4792a96e8b9ccbfa4e0a873d2db199586c562
pl1798 <sploc.work@gmail.com>
2021-05-21 08:44:42 -0700

    n

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class card extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.card');
+    }
+}
