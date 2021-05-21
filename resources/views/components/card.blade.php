4894579afb5b7b7d5c37870e90617b2e35070ff6
pl1798 <sploc.work@gmail.com>
2021-05-21 08:39:03 -0700

    n

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
 </div>
